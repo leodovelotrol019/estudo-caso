90b7af7e3d2336d06c76ab4fc4b01a596013fd50
L3O019 <dadu.tuber@gmail.com>
2026-02-11 17:28:00 -0300

    agrupamento

--- backend/caso8.php
+++ backend/caso8.php
@@ -1,22 +1,22 @@
 <?php
-$precoproduto = "100";
+$precoproduto = $_POST["preco"];
 $precoapos = "";
 $desconto = "";
 if ($precoproduto >= 1000 ) {
    $desconto = 0.15 * $precoproduto;
-   $precoapos = $desconto - $precoproduto; 
+   $precoapos = $precoproduto - $desconto; 
    echo "Após o desconto o preço foi $precoapos";
 }
 elseif ($precoproduto >= 500) {
 
 $desconto = 0.10 * $precoproduto;
-   $precoapos = $desconto - $precoproduto; 
+   $precoapos = $precoproduto - $desconto; 
    echo "Após o desconto o preço foi $precoapos";
 
 }
 else {
    $desconto = 0.05 * $precoproduto;
-   $precoapos = $desconto - $precoproduto; 
+   $precoapos = $precoproduto - $desconto; 
    echo "Após o desconto o preço foi $precoapos";
 }
 
